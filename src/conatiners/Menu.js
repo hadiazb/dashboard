e9daa9f046233b8bb094f35a7e032feeb585de1f
hadiazb <hadiazb@unal.edu.co>
2020-07-21 19:15:05 -0500

    Start Project

---
+++ src/conatiners/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>
+      <div>Option 1</div>
+      <div>Option 2</div>
+      <div>Option 3</div>
+      <div>Option 4</div>
+    </div>
+  );
+};
+
+export default Menu;
