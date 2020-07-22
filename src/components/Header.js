2e5a5ddf87041a7f43c1099d864dcd3baee1d0cb
hadiazb <hadiazb@unal.edu.co>
2020-07-21 23:54:11 -0500

    Mas contenido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import '../assets/components/Header.css';
 import { Link } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import Avatar from '@material-ui/core/Avatar';
 
 const Header = () => {
-  return (
-    <header className='header'>
-      <Link className='header__anchor' to='#'>Estadisticas</Link>
-      <Link className='header__anchor' to='#'>Login</Link>
-    </header>
-  );
+	return (
+		<header className='header'>
+			<Link className='header__anchor' to='/'>
+				<HomeIcon style={{ fontSize: 35 }} />
+			</Link>
+			<div className='header__search'>
+				<Link className='header__anchor' to='/'>
+					<SearchIcon style={{ fontSize: 35 }} />
+				</Link>
+				<Avatar>H</Avatar>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,14 +2,10 @@ import React from 'react';
 import '../assets/components/Main.css';
 
 const Menu = () => {
-  return (
-    <div className='main'>
-      <div>#1</div>
-      <div>#2</div>
-      <div>#3</div>
-      <div>#4</div>
-    </div>
-  );
+	return (
+		<div className='main'>
+		</div>
+	);
 };
 
 export default Menu;
