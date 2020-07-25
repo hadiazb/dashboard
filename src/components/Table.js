83303c683a65d73a8cb1f6d42d140c84fb4a0502
Rafael Lagunas <rafalagunas@MacBook-Pro-de-Rafael.local>
2020-07-25 14:05:03 -0500

    example

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,87 +1,79 @@
-import React from 'react';
-import {
-	withStyles,
-	makeStyles,
-} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Data from '../store/data';
-import '../assets/components/Table.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Data from "../store/data";
+import "../assets/components/Table.css";
+import { Link } from "react-router-dom";
 
 const StyledTableCell = withStyles((theme) => ({
-	head: {
-		backgroundColor: theme.palette.common.black,
-		color: theme.palette.common.white,
-		fontSize: 14,
-	},
-	body: {
-		fontSize: 10,
-	},
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    fontSize: 14,
+  },
+  body: {
+    fontSize: 10,
+  },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
-	root: {
-		'&:nth-of-type(odd)': {
-			backgroundColor: theme.palette.action.hover,
-		},
-	},
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
 }))(TableRow);
 
 const data = Data;
 
 const useStyles = makeStyles({
-	table: {
-		minWidth: 700,
-	},
+  table: {
+    minWidth: 700,
+  },
 });
 
 export default function CustomizedTables() {
-	const classes = useStyles();
-	return (
-		<TableContainer component={Paper}>
-			<Table
-				className={classes.table}
-				aria-label='customized table'
-			>
-				<TableHead>
-					<TableRow>
-						<StyledTableCell>NumeroSIAB</StyledTableCell>
-						<StyledTableCell>Entidad Federativa</StyledTableCell>
-						<StyledTableCell>Municipio</StyledTableCell>
-						<StyledTableCell>Tipo Inmueble</StyledTableCell>
-						<StyledTableCell>Ubicacion</StyledTableCell>
-						<StyledTableCell>Problem Juridica</StyledTableCell>
-						<StyledTableCell>Ver</StyledTableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{data.map((row, key) => (
-						<StyledTableRow key={row.NumeroSIAB}>
-							<StyledTableCell>{row.NumeroSIAB}</StyledTableCell>
-							<StyledTableCell>
-								{row.EntidadFederativa}
-							</StyledTableCell>
-							<StyledTableCell>{row.Municipio}</StyledTableCell>
-							<StyledTableCell>{row.TipoInmueble}</StyledTableCell>
-							<StyledTableCell>{row.Ubicacion}</StyledTableCell>
-							<StyledTableCell>
-								{row.DescripcionProblematicaJuridica}
-							</StyledTableCell>
-							<StyledTableCell>
-								<Link to={`/propiedades/${key}`}>
-									<div className='eye-solid2 icon'></div>
-								</Link>
-							</StyledTableCell>
-						</StyledTableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
+  const classes = useStyles();
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>NumeroSIAB</StyledTableCell>
+            <StyledTableCell>Entidad Federativa</StyledTableCell>
+            <StyledTableCell>Municipio</StyledTableCell>
+            <StyledTableCell>Tipo Inmueble</StyledTableCell>
+            <StyledTableCell>Ubicacion</StyledTableCell>
+            <StyledTableCell>Problem Juridica</StyledTableCell>
+            <StyledTableCell>Ver</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, key) => (
+            <StyledTableRow key={row.NumeroSIAB}>
+              <StyledTableCell>{row.NumeroSIAB}</StyledTableCell>
+              <StyledTableCell>{row.EntidadFederativa}</StyledTableCell>
+              <StyledTableCell>{row.Municipio}</StyledTableCell>
+              <StyledTableCell>{row.TipoInmueble}</StyledTableCell>
+              <StyledTableCell>{row.Ubicacion}</StyledTableCell>
+              <StyledTableCell>
+                {row.DescripcionProblematicaJuridica}
+              </StyledTableCell>
+              <StyledTableCell>
+                <Link to={`/propiedades/${key}`}>
+                  <div className="eye-solid2 icon"></div>
+                </Link>
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
--- src/containers/PropiedadesKey.js
+++ src/containers/PropiedadesKey.js
@@ -1,19 +1,30 @@
-import React, { Fragment } from 'react';
-import Menu from '../components/Menu';
-import Header from '../components/Header';
-import Detalle from '../components/Detalle';
-import '../assets/components/Propiedades.css';
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 
-const Propiedades = () => {
-	return (
-		<Fragment>
-			<Header />
-			<div className='container-tabla'>
-				<Menu />
-				<Detalle />
-			</div>
-		</Fragment>
-	);
-};
+import Menu from "../components/Menu";
+import Header from "../components/Header";
+import Detalle from "../components/Detalle";
+import "../assets/components/Propiedades.css";
+
+class Propiedades extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id_key: this.props.location.pathname.replace("/propiedades/", ""),
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container-tabla">
+          <Menu />
+          <Detalle id={this.state.id_key} />
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Propiedades;
